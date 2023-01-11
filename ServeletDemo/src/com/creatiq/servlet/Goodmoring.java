0278fc89f46ae3857d289d753ea0214b3e3d6897
nuvvulauday2121 <102281701+nuvvulauday2121@users.noreply.github.com>
2023-01-11 13:15:00 +0530

    update data

---
+++ ServeletDemo/src/com/creatiq/servlet/Goodmoring.java
@@ -0,0 +1,24 @@
+package com.creatiq.servlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Goodmoring")
+public class Goodmoring extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		Date date = new Date();
+		pw.println("<h1>good moring evry one today date and time is :"+date+"</h1>");
+	}
+
+}
---
+++ ServeletDemo/src/com/creatiq/servlet/Welcom.java
@@ -0,0 +1,29 @@
+package com.creatiq.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcom
+ */
+@WebServlet("/Welcom")
+public class Welcom extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>Welcom all<h1/>");
+	}
+
+}
